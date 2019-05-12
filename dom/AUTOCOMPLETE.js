e15a1ee80e3164110d348d332df35950889a866b
Rossita <antonelamrossi@gmail.com>
2019-05-12 17:28:02 -0300

    autocomplete

---
+++ dom/AUTOCOMPLETE.js
@@ -0,0 +1,73 @@
+// este es el orden de los eventos
+// keydown el momento en el que la persona empieza a teclear  onkeydown
+// keypress aca la presiona   onkeypress
+// keyup aca dejo de presionar   onkeyup
+
+// en ONKEYUP esta siempre actualizado la info (el valor final) ONKEYDOWN y ONKEYPRESS sse ejecuta antes
+
+// const input = document.getElementById("search");
+
+// input.onkeydown = function () {
+//     console.log("key down", input.value)
+// } 
+
+// input.onkeypress =  function () {
+//     console.log("key press", input.value)
+// }
+
+// input.onkeyup = function () {
+//     console.log("key up", input.value)
+// }
+
+
+
+// 1- en el input agregar onkeyup
+// 2-obtener lo que escribimos en el input
+// 3 generame la URL
+// 4 fetch () devuelve una promise
+// 4.1 res.json()
+// 4.2 hacer un for por todas las pelis que me decuelve la api 
+// 4.3 por cada peli que vamos iterando creamos los lis
+// 4.4 agregar los lis a la UL
+
+
+// https://api.themoviedb.org/3/search/movie?api_key=a70dbfe19b800809dfdd3e89e8532c9e&query=
+
+const input = document.getElementById("autocomplete-input");
+
+input.onkeyup = function () {
+    console.log("key up", input.value)
+    const ul = document.querySelector('ul');  //ul#autocomplete-results
+    
+    if (input.value) {
+
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=a70dbfe19b800809dfdd3e89e8532c9e&query=${input.value}`)
+            .then(res => res.json()) // interprete los datos como json
+            .then(data => {
+                console.log(data, input.value)
+               
+                ul.style.display = "block";
+                let lis = '';
+
+                for (let i = 0; i < data.results.length; i++) {
+
+                    lis +=
+                        `
+                        <li>
+                            <p>${data.results[i].title}</p>
+                        </li>
+                        `;
+                    console.log("algo")
+                }
+
+                ul.innerHTML = lis;
+            });
+    } else {
+        ul.style.display = "none";
+    }
+  
+}
+
+
+
+
--- dom/index.js
+++ dom/index.js
@@ -40,3 +40,23 @@ input.addEventListener('keypress', function (event) {
     }
 
 })
+
+// Codigo del modal para abrir y cerrar
+
+// var botonAbrir = document.getElementById("botonModal");
+// var agarrarModal = document.getElementById("granDiv");
+// var botonCerrar = document.getElementById("cerrarModal");
+// var laCruz = document.getElementById("cruz");
+
+// botonAbrir.onclick = function () {
+//     agarrarModal.classList.remove("displayNone")
+//     botonAbrir.classList.add("displayNone")
+// }
+
+// function cerrar () {
+//     agarrarModal.classList.add("displayNone")
+//     botonAbrir.classList.remove("displayNone")
+// }
+
+// botonCerrar.onclick = cerrar
+// laCruz.onclick = cerrar
